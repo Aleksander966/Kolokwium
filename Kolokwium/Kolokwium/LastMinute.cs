4803e5208e2dd5e647c7cca7aadb406791e0bbee
Aleksander966 <kuciejewski18@gmail.com>
2017-01-16 18:45:22 +0100

    Add files via upload

---
+++ Kolokwium/Kolokwium/LastMinute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium
+{
+    class LastMinute : Podroz
+    {
+        public override double KosztPoRabacie()
+    {
+        return 0.66 * koszt;
+    }
+       
+            
+         
+    }
+}
---
+++ Kolokwium/Kolokwium/Podroz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium
+{
+    class Podroz : IStacja
+    {
+        protected Stack<Stacja> stacje = new Stack<Stacja>();
+        protected double koszt = 100;
+        public Podroz() { }
+        public virtual double KosztPoRabacie()
+        {
+            return 0.9 * koszt;
+        }
+
+        public void DodajStacje(string nazwa, bool oplata)
+        {
+            stacje.Push (new Stacja (nazwa,oplata ));
+            if (oplata == true)
+                koszt += 15;
+
+        }
+
+        public void Powrot()
+        {
+         
+        }
+
+        public void UsunStacje()
+        {
+            stacje.Pop();
+        }
+
+        public void ZapiszPodroz()
+        {
+            
+        }
+    }
+}
